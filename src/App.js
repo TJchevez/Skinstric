3e763b93a3358c92f6dcadcbdfc5fb1f3e49cf63
Thanya <cheveztj@gmail.com>
2025-04-13 19:07:23 -0400

    added font and header image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages.jsx/Home';
 
 function App() {
   return (
-    <div className="App">
-      Sophisticated skincare
-      {/* test */}
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Pages.jsx/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import sophisticatedSkincareHeader from '../images/sophisticatedSkincareHeader.jpg'
+
+function Home() {
+  return (
+    <div className='sophisticatedSkincare__header-home'>
+      <img src={sophisticatedSkincareHeader} alt="" />
+
+      </div>
+
+  )
+};
+
+export default Home;
