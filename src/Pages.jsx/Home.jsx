1c2e17ba83717c7956908c136509773ba55a66fc
Thanya <cheveztj@gmail.com>
2025-04-21 14:25:38 -0400

    home page layout complete

--- src/Pages.jsx/Home.jsx
+++ src/Pages.jsx/Home.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Nav from "../Components.jsx/Nav";
+import takeTestButton from "../assets/takeTestButton.png";
+import discoverButton from "../assets/discoverButton.png";
 
 function Home() {
   return (
     <>
-        <Nav />
+      <Nav />
+
+      <div className="header-line-wrapper">
+        <div className="diamond-left"></div>
+        <img src={discoverButton} alt="" className="discover__button-left" />
+
         <div className="sophisticatedSkincare__header-home">
-          <span className="sophisticatedSkincare__header-font1">Sophisticated </span> 
-          <span className="sophisticatedSkincare__header-font2">Skincare</span>
+          <span className="sophisticatedSkincare__header-font1">
+            Sophisticated
+          </span>
+          <span className="sophisticatedSkincare__header-font2">
+            Skincare
+          </span>
         </div>
-        <ul>
-          <li>
-          <Link to="/Form">Take Test</Link>
-          </li>
-        </ul>
+
+        <div className="diamond-right"></div>
+        <img src={takeTestButton} alt="" className="takeTest__button-right" />
+      </div>
+
+      <p className="skinstric__home-paragraph">
+        Skinstric developed an A.I. that creates a
+        <br />
+        highly-personalized routine tailored to
+        <br />
+        what your skin needs.
+      </p>
     </>
   );
 }
